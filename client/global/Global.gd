c4cb0577dadc862174d413e608424703284b0872
nathan cerisara <cerisara.nathan@gmail.com>
2021-02-17 19:11:32 +0100

    aa

--- client/global/Global.gd
+++ client/global/Global.gd
@@ -1,1 +1,55 @@
 extends Node
+
+var ip = "127.0.0.1"#"192.168.1.53"
+var port = 7896
+
+
+var SOCKET_URL = "ws://"+ip+":"+str(port)
+
+var _client = WebSocketClient.new()
+
+func _ready():
+	_client.connect("connection_closed", self, "_on_connection_closed")
+	_client.connect("connection_error", self, "_on_connection_error")
+	_client.connect("connection_established", self, "_on_connected")
+	_client.connect("data_received", self, "_on_data")
+	
+	var err = _client.connect_to_url(SOCKET_URL)
+	if err != OK:
+		print("Unable to connect to ", SOCKET_URL)
+		set_process(false)
+	else:
+		_send({"test":"test"})
+
+func _process(delta):
+	_client.poll()
+
+func _on_connection_error():
+	print("There was an error")
+	set_process(false)
+
+func _on_connection_closed(was_clean = false):
+	print("Closed, clean: ", was_clean)
+	set_process(false)
+
+func _on_connected(proto = ""):
+	print("Connected with protocol: ", proto)
+
+func _on_data():
+	var payload = JSON.parse(_client.get_peer(1).get_packet().get_string_from_utf8()).result
+	print("Received data: ", payload)
+
+func _send(data, is_json=true):
+	if is_json:
+		data = JSON.print(data)
+	_client.get_peer(1).put_packet(data.to_utf8())
+
+
+
+
+
+
+
+
+
+
